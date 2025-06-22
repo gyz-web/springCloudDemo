46cc5f2e6acd93ff4eee23e93d3c294f17af628b
gyz-web <76725986+gyz-web@users.noreply.github.com>
2025-06-22 15:48:09 +0800

    use nacos config center to get config and auto refresh config.

--- services/service-order/src/main/java/com/guoyongzhuang/order/controller/orderController.java
+++ services/service-order/src/main/java/com/guoyongzhuang/order/controller/orderController.java
@@ -3,15 +3,28 @@ package com.guoyongzhuang.order.controller;
 import com.guoyongzhuang.bean.order.Order;
 import com.guoyongzhuang.order.service.orderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@RefreshScope // 激活配置自动刷新功能
 @RestController
 public class orderController {
 
     @Autowired
     orderService orderService;
+
+    // 获取Nacos中的配置
+    @Value("${order.timeout}")
+    String orderTimeout;
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+    @GetMapping("/getConfig")
+    public String getConfig() {
+        return "orderTimeout: " + orderTimeout +
+                ", orderAutoConfirm: " +orderAutoConfirm ;
+    }
 
     @GetMapping("/create")
     public Order createOrder(@RequestParam Long productId , @RequestParam  Long userId) {
